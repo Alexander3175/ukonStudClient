a34b89492e0dbc41c89821471aaad38ca8291ccd
Sasha <83069448+Alexander3175@users.noreply.github.com>
2024-11-15 22:44:02 +0200

    rename /routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import LoginForm from "./components/auth/LoginForm.tsx";
 import RegForm from "./components/auth/RegForm.tsx";
-import ErrorPage from "./routers/ErrorPage.tsx";
-import MainPage from "./routers/MainPage.tsx";
-import Auth from "./routers/AuthPage.tsx";
+import ErrorPage from "./routers/error.tsx";
+import MainPage from "./routers/main.tsx";
+import Auth from "./routers/auth.tsx";
 
 const routes = createBrowserRouter([
   {
